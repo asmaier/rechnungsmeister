fd0519f8b170994334ec8b28b8539a1c87108448
asmaier <asmaier@web.de>
2016-11-13 23:02:24 +0100

    Adding more than one item to the invoice is now possible.

--- rechnungsmeister.py
+++ rechnungsmeister.py
@@ -33,44 +33,37 @@ class App:
         frame = Frame(master)
         frame.pack()
 
+        self.articles = []
+        self.number_articles = 0
+
         Label(frame, text="Rechnung", font="-weight bold").grid(row=0, columnspan=2)
-        self.invoice_date = self.make_entry2(frame, 1, "Datum:", default="04.11.2017")
-        self.invoice_number = self.make_entry2(frame, 2, "Nummer:", default="TT/1333")
+        self.invoice_date = self.make_entry(frame, 1, "Datum:", default="04.11.2017")
+        self.invoice_number = self.make_entry(frame, 2, "Nummer:", default="TT/1333")
+        self.summary = self.make_entry(frame, 3, "Über:", default="das Urteil mit der Auftragsnummer 12345")
 
-        Label(frame, text="Kunde", font="-weight bold").grid(row=3, columnspan=2)
-        self.customer_title = self.make_entry2(frame, 4, "Titel:", default="geehrter Herr")
-        self.customer_first_name = self.make_entry2(frame, 5, "Vorname:", default="Franz")
-        self.customer_last_name = self.make_entry2(frame, 6, "Nachname", default="Kafka")
-        self.customer_street = self.make_entry2(frame, 7, "Strasse:", default="No-Name-Straße 2a")
-        self.customer_city = self.make_entry2(frame, 8, "Stadt:", default="12345 Prag")
+        Label(frame, text="Kunde", font="-weight bold").grid(row=4, columnspan=2)
+        self.customer_title = self.make_entry(frame, 5, "Titel:", default="geehrter Herr")
+        self.customer_first_name = self.make_entry(frame, 6, "Vorname:", default="Franz")
+        self.customer_last_name = self.make_entry(frame, 7, "Nachname", default="Kafka")
+        self.customer_street = self.make_entry(frame, 8, "Strasse:", default="No-Name-Straße 2a")
+        self.customer_city = self.make_entry(frame, 9, "Stadt:", default="12345 Prag")
 
-        Label(frame, text="Artikel", font="-weight bold").grid(row=9, columnspan=2)
-        self.item_long = self.make_entry2(frame, 10, "Artikel (lang):", default="das Urteil mit der Auftragsnummer 12345")
-        self.item = self.make_entry2(frame, 11, "Artikel:", default="Urteil, 70 Cent pro Zeile")
-        self.quantity = self.make_entry2(frame, 12, "Menge:", default="100")
-        self.price = self.make_entry2(frame, 13, "Preis:", default="0,70")
+        subframe = Frame(frame)
+        self.add_article(subframe, 0)
+        subframe.grid(row=11, columnspan=2)
 
         self.quit = Button(frame, text="Schliessen", font="-weight bold", command=frame.quit)
-        self.quit.grid(row=14, column=0)
+        self.quit.grid(row=12, column=0)
+        self.article = Button(frame, text="Artikel hinzufügen", font="-weight bold", command=lambda: self.add_article(subframe, 0))
+        self.article.grid(row=12, column=1)
         self.generate = Button(frame, text="Erstelle Rechnung", font="-weight bold", command=self.generate_invoice)
-        self.generate.grid(row=14, column=1, sticky=E)
+        self.generate.grid(row=12, column=2, sticky=E)
 
         self.parser = SafeConfigParser()
         self.parser.read("rechnung.ini")
 
-    def make_entry(self, parent, caption, width=None, default=None, **options):
-        fm = Frame(parent)
-        Label(fm, text=caption, width=10).pack(side=LEFT)
-        entry = Entry(fm, **options)
-        if width:
-            entry.config(width=width)
-        if default:
-            entry.insert(0, default)
-        entry.pack(side=LEFT)
-        fm.pack(side=TOP, anchor=W, fill=X, expand=YES)
-        return entry
 
-    def make_entry2(self, parent, row, label, default=None):
+    def make_entry(self, parent, row, label, default=None):
         Label(parent, text=label).grid(row=row, column=0, sticky=W)
         entry = Entry(parent)
         if default:
@@ -79,33 +72,44 @@ class App:
         entry.grid(row=row, column=1)
         return entry
 
+    def add_article(self, frame, row):
+        self.number_articles += 1
+
+        start_row = row + (self.number_articles - 1) * 4
+
+        print "Adding article ", self.number_articles
+        Label(frame, text="Artikel", font="-weight bold").grid(row=start_row, columnspan=2)
+        item = self.make_entry(frame, start_row + 1, "Artikel:", default="Urteil, 70 Cent pro Zeile")
+        quantity = self.make_entry(frame, start_row + 2, "Menge:", default="100")
+        price = self.make_entry(frame, start_row + 3, "Preis:", default="0,70")
+
+        self.articles.append((item, quantity, price))
+
     def generate_invoice(self):
         template = latex_jinja_env.get_template('rechnung_template.tex')
 
         variables = {
-            "biller_name_long" : self.parser.get("biller","name_long"),
-            "biller_name" : self.parser.get("biller","name"),
-            "biller_street" : self.parser.get("biller","street"),
-            "biller_city" : self.parser.get("biller","city"),
-            "biller_phone" : self.parser.get("biller","phone"),
-            "biller_email" : self.parser.get("biller","email"),
-            "biller_vat" : self.parser.get("biller","vat_number"),
-            "bank_name" : self.parser.get("bank","name"),
-            "bank_code" : self.parser.get("bank","code"),
-            "bank_account" : self.parser.get("bank","account"),
-            "bank_iban" : self.parser.get("bank","iban"),
-            "bank_bic" : self.parser.get("bank","bic"),
-            "invoice_number" : self.invoice_number.get(),
-            "invoice_date" : self.invoice_date.get(),
-            "customer_title" : self.customer_title.get(),
-            "customer_first_name" : self.customer_first_name.get(),
-            "customer_last_name" : self.customer_last_name.get(),
-            "customer_street" : self.customer_street.get(),
-            "customer_city" : self.customer_city.get(),
-            "item_long" : self.item_long.get(),
-            "item" : self.item.get(),
-            "quantity" : self.quantity.get(),
-            "price" : self.price.get()
+            "biller_name_long": self.parser.get("biller","name_long"),
+            "biller_name": self.parser.get("biller","name"),
+            "biller_street": self.parser.get("biller","street"),
+            "biller_city": self.parser.get("biller","city"),
+            "biller_phone": self.parser.get("biller","phone"),
+            "biller_email": self.parser.get("biller","email"),
+            "biller_vat": self.parser.get("biller","vat_number"),
+            "bank_name": self.parser.get("bank","name"),
+            "bank_code": self.parser.get("bank","code"),
+            "bank_account": self.parser.get("bank","account"),
+            "bank_iban": self.parser.get("bank","iban"),
+            "bank_bic": self.parser.get("bank","bic"),
+            "invoice_number": self.invoice_number.get(),
+            "invoice_date": self.invoice_date.get(),
+            "customer_title": self.customer_title.get(),
+            "customer_first_name": self.customer_first_name.get(),
+            "customer_last_name": self.customer_last_name.get(),
+            "customer_street": self.customer_street.get(),
+            "customer_city": self.customer_city.get(),
+            "summary" : self.summary.get(),
+            "articles": [(item.get(), quantity.get(), price.get()) for (item, quantity, price) in self.articles]
         }
 
         with open("rechnung.tex", "w") as out_file:
